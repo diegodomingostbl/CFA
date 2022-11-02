18381c69a3c5f15626761a67d2b961b4c7295a59
ShiguemiAlexandre <53339523+ShiguemiAlexandre@users.noreply.github.com>
2022-11-02 19:14:23 -0300

    Tela de cadastro de visitante

--- main.c
+++ main.c
@@ -1,64 +1,51 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <locale.h>
-#include <string.h>
-#include <stdbool.h>
+#include<stdio.h>
 
-#include "archive.h"
-#include "funcoes.h"
-
-USERLOGIN usuarioLogado;
-
-USERLOGIN login(char* error){
-    USERLOGIN uLogin;
-    char c;
-    int a=0;
-
-    printf("Entre com o login: ");
-    gets(uLogin.login);
-    printf("Entre com a senha: ");
-    do{
-        c=getch();
-        if(isprint(c)){
-            uLogin.senha[a] = c;
-            a++;
-            printf("*");
-        }else if(c == 8 && a){
-            uLogin.senha[a] = '\0';
-            a--;
-            printf("\b \b");
-        }
-    }while(c != 13);
-    uLogin.senha[a]='\0';
-
-    if(strcmp(uLogin.login, "") && strcmp(uLogin.senha, "")){
-        uLogin = realizarLogin(uLogin, error);
-    }else{
-        strcpy(error, "Credenciais inválidas");
-        uLogin.logado = false;
-        strcpy(uLogin.senha, "");
-    }
-
-    return uLogin;
-}
-
-void loginMain(){
-    USER usuario;
-    char* error = malloc(150 * sizeof(char));
-
-    usuarioLogado = login(error);
-    printf("\n");
-
-    if(usuarioLogado.logado == true){
-        printf("Usuário %s logado com sucesso\n", usuarioLogado.nome);
-    }else{
-        printf("Error: %s\n", error);
-    }
-
-    retornaUsuarioLogado();
-}
+struct motorista {
+    char nome [50];
+    char motorista_rg_cpf_cnh[15];
+    char bloco_casas[10];
+    char placa[10];
+};
 
 void main(){
-    setlocale(LC_ALL, "Portuguese");
-    loginMain();
+	int opcao;
+    struct motorista motorista;
+	do{
+        printf("*Cadastro de visitante*");
+		printf("\n[1] - Motorista");
+		printf("\n[2] - Pedestre");
+		printf("\n O que deseja cadastrar: ");
+		scanf("%d", &opcao);
+		switch(opcao){
+			case 1:
+			    fflush(stdin);
+			    fflush(stdout);
+			    system("cls");
+				printf("\n *Cadastro de motorista*");
+				printf("\n Informe o nome completo do visitante: ");
+				gets(motorista.nome);
+				printf("\n Informe o RG, CPF ou CNH: ");
+				gets(motorista.motorista_rg_cpf_cnh);
+				printf("\n Bloco ou numero da casa reponsável: ");
+				gets(motorista.bloco_casas);
+				printf("\n Dados da placa: ");
+				gets(motorista.placa);
+				break;
+			case 2:
+                fflush(stdin);
+                fflush(stdout);
+			    system("cls");
+				printf("\n *Cadastro de pedestre*");
+				printf("\n Informe o nome completo do visitante: ");
+				gets(motorista.nome);
+				printf("\n Informe o RG, CPF ou CNH: ");
+				gets(motorista.motorista_rg_cpf_cnh);
+				printf("\n Bloco ou numero da casa reponsável: ");
+				gets(motorista.bloco_casas);
+				break;
+			default:
+			    system("cls");
+				printf("\nOpcao invalida");
+		}
+	}while(opcao < 1 || opcao > 2);
 }
